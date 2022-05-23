0f57021830224c067e44c60f766dfe219c7a8561
okewunmi <okewunmiafeezolaide@gmail.com>
2022-05-23 21:37:30 +0100

    first commit

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,2 +1,25 @@
 import { createGlobalStyle } from "styled-components";
-const
+
+export const GlobalStlye = createGlobalStyle `
+
+:root{
+    --maxwidth: 1280px;
+    --white: #ffff;
+    --lightGrey: #eee;
+    --MedGrey:#353535;
+    --darkGrey: #1c1c1c;
+    --fontSuperBig: 2.5rem;
+    --fontBig: 1.5rem;
+    --fontMedium: 1.2rem;
+    --fontSmall: 1rem   
+}
+*{
+    box-sizing: boarder-box;
+    pading: 0;
+    margin: 0;
+    
+}
+
+
+
+`
